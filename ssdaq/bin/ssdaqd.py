cc8c442486a8592efb44cd124f9137bca2f4e1df
Samuel Flis <samuel.d.flis@gmail.com>
2018-10-18 15:49:53 +0200

    Added argparser to ssdaqd.py

--- ssdaq/bin/ssdaqd.py
+++ ssdaq/bin/ssdaqd.py
@@ -4,11 +4,27 @@ from ssdaq.core.SSEventDataPublisher import SSEventDataPublisher
 
 import sys
 import time
-listen_port = sys.argv[1]
-publicate_port = sys.argv[2]
-eb = SSEventBuilder()
-dl = SSDataListener(listen_port,eb.data_queue)
-ep = SSEventDataPublisher(publicate_port,eb.event_queue)
+
+import argparse
+
+parser = argparse.ArgumentParser(description='Start slow signal data acquisition.',formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+
+parser.add_argument('-l', dest='listen_port', type=str,
+                    default='2009',
+                    help='port for incoming data')
+
+parser.add_argument('-p', dest='publisher_port', type=str,
+                    default='5555',
+                    help='port for publishing data')
+
+parser.add_argument('-V','--verbose', dest='verbose', action='store_true',
+                    help='Turn on verbose mode')
+
+args = parser.parse_args()
+
+eb = SSEventBuilder(args.verbose)
+dl = SSDataListener(args.listen_port,eb.data_queue)
+ep = SSEventDataPublisher(args.publishing_port,eb.event_queue)
 
 dl.start()
 time.sleep(1)
