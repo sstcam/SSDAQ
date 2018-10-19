c71ceeb6675b0bf847131bc8c2bd65b19cf38889
Samuel Flis <samuel.d.flis@gmail.com>
2018-10-19 11:44:32 +0200

    Convert tabs to spaces

--- ssdaq/bin/simpleSSEventListener.py
+++ ssdaq/bin/simpleSSEventListener.py
@@ -20,20 +20,20 @@ n_modules_per_event =[]
 n = 0
 while(True):
 
-	event = ev_list.event_buffer.get()
-	
-	print("Event number %d run number %d"%(event.event_number,event.run_number))
-	m = event.timestamps[:,0]>0
-	print(np.sum(m))
-	print(np.where(m)[0])
-	n_modules_per_event.append(np.sum(m))
-	print((event.timestamps[m][0]-event.timestamps[m])*1e-7)
-	event_counter[m] += 1
-	m = event_counter>0
-	print(list(zip(np.where(m)[0],event_counter[m])))
-	if(n>2000):
-		break
-	n +=1
+    event = ev_list.event_buffer.get()
+    
+    print("Event number %d run number %d"%(event.event_number,event.run_number))
+    m = event.timestamps[:,0]>0
+    print(np.sum(m))
+    print(np.where(m)[0])
+    n_modules_per_event.append(np.sum(m))
+    print((event.timestamps[m][0]-event.timestamps[m])*1e-7)
+    event_counter[m] += 1
+    m = event_counter>0
+    print(list(zip(np.where(m)[0],event_counter[m])))
+    if(n>2000):
+        break
+    n +=1
 plt.figure()
 plt.hist(n_modules_per_event, 10,  facecolor='g', alpha=0.75)
 plt.show()
