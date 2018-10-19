9c99ee0c4e4420a14d44ec0a3c0122f227fef852
Samuel Flis <samuel.d.flis@gmail.com>
2018-10-19 11:43:40 +0200

    Convert tabs to spaces

--- ssdaq/core/SSEventListener.py
+++ ssdaq/core/SSEventListener.py
@@ -5,21 +5,21 @@ import zmq
 from queue import Queue
 
 class SSEventListener(Thread):
-	def __init__(self, port = '5555'):
-		Thread.__init__(self)
-		
-		self.context = zmq.Context()
-		self.sock = self.context.socket(zmq.SUB)
-		self.sock.setsockopt(zmq.SUBSCRIBE, b"")
-		self.sock.connect("tcp://127.0.0.101:"+port)
-		self.running = False
-		self.event_buffer = Queue()
-	
-	def run(self):
-		self.running = True
+    def __init__(self, port = '5555'):
+        Thread.__init__(self)
+        
+        self.context = zmq.Context()
+        self.sock = self.context.socket(zmq.SUB)
+        self.sock.setsockopt(zmq.SUBSCRIBE, b"")
+        self.sock.connect("tcp://127.0.0.101:"+port)
+        self.running = False
+        self.event_buffer = Queue()
+    
+    def run(self):
+        self.running = True
 
-		while(self.running):
-			data = self.sock.recv()
-			event = SSEventBuilder.SSEvent()
-			event.unpack(data)
-			self.event_buffer.put(event)
+        while(self.running):
+            data = self.sock.recv()
+            event = SSEventBuilder.SSEvent()
+            event.unpack(data)
+            self.event_buffer.put(event)
