ab40416f5eef548305b13179a6be1cbfe3daeb1f
Samuel Flis <samuel.d.flis@gmail.com>
2018-10-20 00:15:55 +0200

    Trimmed the simulation code

--- tm-ss-sim/SSUDPTMSimulator.py
+++ tm-ss-sim/SSUDPTMSimulator.py
@@ -14,26 +14,19 @@ class TMSimulator(object):
     def __init__(self, port, ip):
         self.port = port
         self.host_ip = ip
-        self.data_prim = np.random.uniform(0,400,32)
-        self.data_aux = np.random.uniform(0,400,32)
+        self.data = np.random.uniform(0,400,64)
 
     def simulate_data(self):
         #simulate next step
-        self.data_prim += np.random.uniform(-4,4,32)
-        self.data_prim[self.data_prim<0] = 0
-        self.data_aux += np.random.uniform(-4,4,32)
-        self.data_aux[self.data_aux<0] = 0
+        self.data += np.random.uniform(-4,4,64)
+        self.data[self.data<0] = 0
         
         #convert mV to counts and correct bit format
-        prim_counts = np.asarray(self.data_prim/(0.03815*2.),dtype=np.uint16)
-        m = prim_counts<0x8000
-        prim_counts[m] +=0x8000
-        prim_counts[~m]= prim_counts[~m]&0x7FFF
-        aux_counts = np.asarray(self.data_aux/(0.03815*2.),dtype=np.uint16)
-        m = aux_counts<0x8000
-        aux_counts[m] +=0x8000
-        aux_counts[~m]= aux_counts[~m]&0x7FFF
-        return prim_counts.copy(),aux_counts.copy()
+        data_counts = np.asarray(self.data/(0.03815*2.),dtype=np.uint16)
+        m = data_counts<0x8000
+        data_counts[m] +=0x8000
+        data_counts[~m]= data_counts[~m]&0x7FFF
+        return data_counts.copy()#,aux_counts.copy()
 
     def Run(self):
         sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -47,13 +40,13 @@ class TMSimulator(object):
             timestamp = t.timestamp() + np.random.uniform(-2e-4,2e-4)
             data_packet = DataStream()
             for i in range(10):
-                prim, aux = self.simulate_data()
+                data = self.simulate_data()
                 data_packet.append(int((timestamp+0.1*i)*1e9),'Q')
-                for ss_p in prim:
-                    data_packet.append(ss_p,'H')
+                for ss_d in data[:32]:
+                    data_packet.append(ss_d,'H')
                 data_packet.append(int((timestamp+0.1*i)*1e9),'Q')
-                for ss_a in aux:
-                    data_packet.append(ss_a,'H')
+                for ss_d in data[32:]:
+                    data_packet.append(ss_d,'H')
                 nreading += 1
 
             sent = sock.sendto(data_packet, server_address)
