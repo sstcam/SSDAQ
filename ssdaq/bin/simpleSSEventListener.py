bf9d6596cb157553e2e84334eed22866d560ab07
Samuel Flis <samuel.d.flis@gmail.com>
2018-10-25 11:09:03 +0200

    - The SSEventListener class is updated so it can gracefully exit the listening thread through the new method CloseThread. - Made changes to the simpleSSEventListener.py script to adapt to the above changes to make it exit gracefully when hitting ctrl+C

--- ssdaq/bin/simpleSSEventListener.py
+++ ssdaq/bin/simpleSSEventListener.py
@@ -1,10 +1,14 @@
 from ssdaq.core import SSEventListener
 import numpy as np
 from matplotlib import pyplot as plt
-
 import argparse
 
-parser = argparse.ArgumentParser(description='Start a simple event data listener.',formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+import signal
+import sys
+
+
+parser = argparse.ArgumentParser(description='Start a simple event data listener.',
+                                formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 
 parser.add_argument('-l', dest='listen_port', type=str,
                     default='5555',
@@ -15,19 +19,37 @@ args = parser.parse_args()
 ev_list = SSEventListener.SSEventListener(args.listen_port)
 ev_list.start()
 
+# def signal_handler(sig, frame):
+        
+#         ctrc_count +=1
+    
+#         ev_list.running=False
+#         print('\npressed ctrl-C')
+#         ev_list.CloseThread()
+#         print("Closing listener")
+#         raise RuntimeError
+
+# signal.signal(signal.SIGINT, signal_handler)
+
 event_counter = np.zeros(32)
 n_modules_per_event =[]
 n = 0
+signal.alarm(0)
+ctrc_count = 0
 while(True):
+    try:
+        event = ev_list.GetEvent()
+    except :
+        print("\nClosing listener")
+        ev_list.CloseThread()
+        break
 
-    event = ev_list.event_buffer.get()
-    
     print("Event number %d run number %d"%(event.event_number,event.run_number))
     m = event.timestamps[:,0]>0
     print(np.sum(m))
     print(np.where(m)[0])
     n_modules_per_event.append(np.sum(m))
-    print((event.timestamps[m][0]-event.timestamps[m])*1e-7)
+    print((event.timestamps[m][0,0]*1e-9-event.timestamps[m][:,0]*1e-9))
     event_counter[m] += 1
     m = event_counter>0
     print(list(zip(np.where(m)[0],event_counter[m])))
@@ -37,3 +59,5 @@ while(True):
 plt.figure()
 plt.hist(n_modules_per_event, 10,  facecolor='g', alpha=0.75)
 plt.show()
+ev_list.CloseThread()
+ev_list.join()
--- ssdaq/core/SSEventListener.py
+++ ssdaq/core/SSEventListener.py
@@ -5,6 +5,7 @@ import zmq
 from queue import Queue
 
 class SSEventListener(Thread):
+    id_counter = 0
     def __init__(self, port = '5555'):
         Thread.__init__(self)
         
@@ -13,13 +14,47 @@ class SSEventListener(Thread):
         self.sock.setsockopt(zmq.SUBSCRIBE, b"")
         self.sock.connect("tcp://127.0.0.101:"+port)
         self.running = False
-        self.event_buffer = Queue()
-    
+        self._event_buffer = Queue()
+        SSEventListener.id_counter += 1
+        self.id_counter = SSEventListener.id_counter
+        self.inproc_sock_name = "SSEventListener%d"%(self.id_counter) 
+        self.close_sock = self.context.socket(zmq.PAIR)
+        self.close_sock.bind("inproc://"+self.inproc_sock_name)
+
+    def CloseThread(self):
+        if(self.running):
+            self.close_sock.send(b"close")
+        # #Empty the buffer after closing the recv thread
+        while(not self._event_buffer.empty()):
+            self._event_buffer.get()
+            self._event_buffer.task_done()
+        self._event_buffer.join()
+
+    def GetEvent(self,**kwargs):
+        event = self._event_buffer.get(**kwargs)
+        self._event_buffer.task_done()       
+        return event
+
     def run(self):
+        print('Starting listener')
+        recv_close = self.context.socket(zmq.PAIR)
+        recv_close.connect("inproc://"+self.inproc_sock_name)
         self.running = True
 
+        poller = zmq.Poller()
+        poller.register(self.sock,zmq.POLLIN)
+        poller.register(recv_close,zmq.POLLIN)
+
         while(self.running):
-            data = self.sock.recv()
-            event = SSEventBuilder.SSEvent()
-            event.unpack(data)
-            self.event_buffer.put(event)
+            
+            socks= dict(poller.poll())
+            
+            if(self.sock in socks):
+                data = self.sock.recv()
+                event = SSEventBuilder.SSEvent()
+                event.unpack(data)
+                self._event_buffer.put(event)
+            else:
+                print('Stopping')
+                break
+        self.running = False
