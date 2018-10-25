8bf758649c639687aeb7a6867167f0415dcb3cc1
Samuel Flis <samuel.d.flis@gmail.com>
2018-10-25 11:16:09 +0200

    Added a first version of an eventdata file writer class with a corresponding script to execute it.

---
+++ ssdaq/bin/ssdatawriter.py
@@ -0,0 +1,34 @@
+from ssdaq.event_receivers import EventFileWriter
+
+import argparse
+
+# import signal
+# import sys
+
+
+parser = argparse.ArgumentParser(description='Start a simple event data listener.',
+                                formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+
+parser.add_argument('-l', dest='listen_port', type=str,
+                    default='5555',
+                    help='port for incoming event data')
+
+parser.add_argument('filename', type=str,
+                    default='5555',
+                    help='Output file name')
+
+args = parser.parse_args()
+
+
+data_writer = EventFileWriter.EventFileWriter(args.filename)
+
+data_writer.start()
+
+running = True
+while(running):
+    ans = input('To stop type `yes`:')
+    if(ans == 'yes'):
+        data_writer.running = False
+        running = False
+data_writer.join()
+
---
+++ ssdaq/event_receivers/EventFileWriter.py
@@ -0,0 +1,45 @@
+from ssdaq.core import SSEventListener
+from threading import Thread
+import numpy as np
+import tables
+from tables import IsDescription,open_file,UInt64Col,Float64Col
+
+
+class SSEventTableDs(IsDescription):
+    event_number = UInt64Col()
+    runt_number = UInt64Col()
+    ev_time = UInt64Col()
+    data = Float64Col((32,64))
+    time_stamps = UInt64Col((32,2))
+
+class EventFileWriter(Thread):
+    def __init__(self, filename):
+        Thread.__init__(self)
+        self.filename = filename
+        self.event_listener = SSEventListener.SSEventListener()
+        self.file = open_file(filename, mode="w", title="CHEC-S Slow signal monitor data")
+        self.group = self.file.create_group("/", 'SlowSignal', 'Slow signal data')
+        self.table = self.file.create_table(self.group, 'readout', SSEventTableDs, "Slow signal readout")
+        self.running = False
+        self.event_counter = 0
+    def run(self):
+        self.event_listener.start()
+        self.running = True
+        ev_row = self.table.row  
+        while(self.running):
+            event = self.event_listener.GetEvent()
+            ev_row['event_number'] = event.event_number
+            ev_row['ev_time'] = event.event_timestamp
+            ev_row['data'] = event.data
+            ev_row['time_stamps'] = event.timestamps
+            ev_row.append()
+            self.table.flush()
+            self.event_counter +=1
+            print(self.event_counter)
+        self.event_listener.CloseThread()
+        self.file.close()
+
+
+
+
+
