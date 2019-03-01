34f1df4259a5ee30e0ce31ee714308236f2f99c9
Samuel Flis <samuel.d.flis@gmail.com>
2019-03-01 13:14:33 +0100

    Removed redundant test

--- ssdaq/tests/test_ss_event_builder.py
+++ ssdaq/tests/test_ss_event_builder.py
@@ -6,40 +6,40 @@ import socket
 from queue import Queue
 import time
 
-#Silencing INFO logging
-import logging
-from ssdaq import sslogger
-sslogger.setLevel(logging.ERROR)
+# #Silencing INFO logging
+# import logging
+# from ssdaq import sslogger
+# sslogger.setLevel(logging.ERROR)
 
-import numpy as np
-class TestSSReadout(unittest.TestCase):
-    def test_pack_unpack(self):
-        readout1 = SSReadout(timestamp = 12345,readout_number=1234)
-        readout1.data[2,:] = np.arange(64)
-        readout1.data[3,:] = np.arange(64)
-        readout1.timestamps[:,0] = np.arange(32,dtype=np.uint64)
-        packed_readout = readout1.pack()
-        readout2 = SSReadout()
-        readout2.unpack(packed_readout)
+# import numpy as np
+# class TestSSReadout(unittest.TestCase):
+#     def test_pack_unpack(self):
+#         readout1 = SSReadout(timestamp = 12345,readout_number=1234,cpu_t=1234)
+#         readout1.data[2,:] = np.arange(64)
+#         readout1.data[3,:] = np.arange(64)
+#         # readout1.timestamps[:,0] = np.arange(32,dtype=np.uint64)
+#         packed_readout = readout1.pack()
+#         readout2 = SSReadout()
+#         readout2.unpack(packed_readout)
 
-        self.assertEqual(readout1.readout_number , readout2.readout_number)
-        self.assertEqual(readout1.readout_timestamp , readout2.readout_timestamp)
-        self.assertTrue((readout1.data[2]==readout2.data[2]).all())
-        self.assertTrue((readout1.data[3]==readout2.data[3]).all())
-        self.assertFalse((readout1.data[4]==readout2.data[4]).all())
-        self.assertTrue(np.isnan(readout1.data[4,0]))
-        self.assertTrue((readout1.timestamps[0]==readout2.timestamps[0]).all())
+#         self.assertEqual(readout1.readout_number , readout2.readout_number)
+#         self.assertEqual(readout1.readout_timestamp , readout2.readout_timestamp)
+#         self.assertTrue((readout1.data[2]==readout2.data[2]).all())
+#         self.assertTrue((readout1.data[3]==readout2.data[3]).all())
+#         self.assertFalse((readout1.data[4]==readout2.data[4]).all())
+#         self.assertTrue(np.isnan(readout1.data[4,0]))
+#         # self.assertTrue((readout1.timestamps[0]==readout2.timestamps[0]).all())
 
 
 # from ssdaq.core.SSReadoutAssembler import SSReadoutAssembler
 # class TestSSReadoutAssembler(unittest.TestCase):
-    
+
 #     def test_out_of_range_ip(self):
 #         sslogger.setLevel(logging.FATAL)
 #         import numpy as np
 #         import struct
 #         builder = SSReadoutAssembler()
-#         try:    
+#         try:
 #             builder.start()
 #             data_packet = bytearray()
 #             data_packet.extend(struct.pack('>Q',192093))
@@ -49,14 +49,14 @@ class TestSSReadout(unittest.TestCase):
 #             builder.data_queue.put(('192.168.0.132',data_packet))
 #             builder.running = False
 #             builder.data_queue.put(('192.168.0.132',data_packet))
-#             builder.join()        
+#             builder.join()
 #         except :
 #             self.assertTrue(True)
 #         else:
 #             self.assertTrue(False,'IP out of range not allowed without relaxed_ip_range set to True')
 
 #         builder = SSReadoutAssembler(relaxed_ip_range = True)
-#         try:    
+#         try:
 #             builder.start()
 #             data_packet = bytearray()
 #             data_packet.extend(struct.pack('>Q',192093))
@@ -66,7 +66,7 @@ class TestSSReadout(unittest.TestCase):
 #             builder.data_queue.put(('192.168.0.132',data_packet))
 #             builder.running = False
 #             builder.data_queue.put(('192.168.0.132',data_packet))
-#             builder.join()        
+#             builder.join()
 #         except :
 #             self.assertTrue(False,'IP out of range allowed with relaxed_ip_range set to True')
 #         else:
