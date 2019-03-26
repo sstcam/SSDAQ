d31fb31f88f49f2aafa30b2f53d983f6e2033b57
Samuel Flis <samuel.d.flis@gmail.com>
2019-03-26 14:59:28 +0100

    Implemented a trigger packet listener and a basic listener which can replace the ssreadoutlistener

--- ssdaq/core/triggers/trigger_listener.py
+++ ssdaq/core/triggers/trigger_listener.py
@@ -34,24 +34,24 @@ class TriggerPacketListener:
         self.running = True
         self.publishers = publishers
         self.listen_addr = (ip,port)
-        self.corrs = [self.relay]
+        self.corrs = [self.relay()]
         self.loop = asyncio.get_event_loop()
-        self.tpp = TriggerPacketProtocol(self.loop,self.log)
+        # self.tpp = TriggerPacketProtocol(self.loop,self.log)
         for p in self.publishers:
-            p.give_loop(self.loop)
+            p.set_loop(self.loop)
 
     def run(self):
         self.log.info('Settting up listener at %s:%d'%(tuple(self.listen_addr)))
         listen = self.loop.create_datagram_endpoint(
-                lambda :SlowSignalDataProtocol(self.loop,self.log),
+                lambda :TriggerPacketProtocol(self.loop,self.log),
                 local_addr=self.listen_addr)
 
-        ransport, protocol = self.loop.run_until_complete(listen)
+        transport, protocol = self.loop.run_until_complete(listen)
+        self.tpp = protocol
         self.ss_data_protocol = protocol
         self.log.info('Number of publishers registered %d'%len(self.publishers))
         for c in self.corrs:
             self.loop.create_task(c)
-
         try:
             self.loop.run_forever()
         except:
@@ -66,4 +66,7 @@ class TriggerPacketListener:
 
 
 
-
+if(__name__ == "__main__"):
+    from ssdaq.core import publishers
+    trpl = TriggerPacketListener(port=2009,ip='0.0.0.0',publishers = [publishers.ZMQTCPPublisher(ip='127.0.0.101',port=5555)])
+    trpl.run()
