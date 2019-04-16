6d4925d8886f7ed7f2f986933f84168e9dfc6e4f
Samuel Flis <samuel.d.flis@gmail.com>
2019-04-16 18:32:03 +0200

    add monitor receiver to module

--- ssdaq/receivers.py
+++ ssdaq/receivers.py
@@ -2,3 +2,4 @@ from .core.slowsignal.readout_assembler import SSReadoutAssembler
 from .core.timestamps.timestamp_receiver import TimestampReceiver
 from .core.logging.log_receiver import LogReceiver
 from .core.triggers.trigger_receiver import TriggerPacketReceiver
+from .core.monitor.monitor_receiver import MonitorReceiver
