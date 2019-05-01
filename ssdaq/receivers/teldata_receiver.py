f21da5192530662e52579cf034217387d8f99f8a
Samuel Flis <samuel.d.flis@gmail.com>
2019-05-01 21:20:31 +0200

    Add the actual mysql query code to the TelDataReceiver

--- ssdaq/receivers/teldata_receiver.py
+++ ssdaq/receivers/teldata_receiver.py
@@ -4,8 +4,9 @@ from .mon_sender import ReceiverMonSender
 from collections import deque
 from ssdaq.data import TelData
 from ssdaq.core.utils import get_utc_timestamp
-
-
+import MySQLdb
+from datetime import datetime
+import os
 class TelDataReceiver(ReceiverServer):
     def __init__(
         self, ip: str, port: int, publishers: list, name: str = "TelDataReceiver"
@@ -21,14 +22,28 @@ class TelDataReceiver(ReceiverServer):
         self.log.info("Starting teldb query task")
         while self.running:
 
-            # query data base Dummy code for now
             teldata = TelData()
-            sec, nsec = get_utc_timestamp()
-            teldata.time.sec = sec
-            teldata.time.nsec = nsec
-            teldata.ra = 0.231
-            teldata.dec = -0.231
-            self.log.info("Created fake tel data")
+            if os.uname().nodename == "chec1.mpi-hd.mpg.de":
+                db = MySQLdb.connect(host="slntmcdb.astrivpn.com",user="ASTRI",passwd="ASTRIteam2014",db="monitoring")
+                cursor = db.cursor()
+                # cursor.execute("SELECT TCU_ACTUAL_DEC.timetag, TCU_ACTUAL_DEC.value, TCU_ACTUAL_RA.value  FROM TCU_ACTUAL_DEC, TCU_ACTUAL_RA WHERE timetag=(SELECT MAX(timetag) FROM TCU_ACTUAL_DEC);")
+                cursor.execute("SELECT *  FROM TCU_ACTUAL_DEC WHERE timetag=(SELECT MAX(timetag) FROM TCU_ACTUAL_DEC);")
+                dec = cursor.fetchall()
+                cursor.execute("SELECT *  FROM TCU_ACTUAL_RA WHERE timetag=(SELECT MAX(timetag) FROM TCU_ACTUAL_RA);")
+                ra = cursor.fetchall()
+                sec = datetime.utcfromtimestamp(ra[0][0]/10000000-12219292800)
+                teldata.time.sec = sec
+                teldata.time.nsec = 0
+                teldata.ra = float(ra[0][1])
+                teldata.dec = float(dec[0][1])
+            else:
+                sec,nsec = get_utc_timestamp()
+                teldata.time.sec = sec
+                teldata.time.nsec = nsec
+                teldata.ra = 0.322
+                teldata.dec = -0.24
+                self.log.info("Created fake tel data")
+
             self.mon.register_data_packet()
             await self.publish(teldata.SerializeToString())
             await asyncio.sleep(15)
